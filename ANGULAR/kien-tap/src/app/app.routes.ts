566987990f70f28e0ed1ce41cb13ed8d1fbb6dd4
Quynh Nhu <nhuntq23406@st.uel.edu.vn>
2026-05-16 18:09:35 +0700

    update giao dien

--- ANGULAR/kien-tap/src/app/app.routes.ts
+++ ANGULAR/kien-tap/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./featured/admin/routes/admin.routes').then(m => m.ADMIN_ROUTES)
+  },
+  {
+    path: '',
+    redirectTo: 'admin',
+    pathMatch: 'full'
+  }
+];
