2317a68ba599599eced4671a2717485a443c11d1
PhuongDo186 <phuongdt23406@st.uel.edu.vn>
2026-05-23 11:08:53 +0700

    feat: trien khai module bao cao backend va bat dau tich hop frontend

--- ANGULAR/backend/src/app.module.ts
+++ ANGULAR/backend/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { TicketsModule } from './tickets/tickets.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ChinhSachModule } from './chinh-sach/chinh-sach.module';
+import { BaoCaoModule } from './bao-cao/bao-cao.module';
 
 @Module({
-  imports: [PrismaModule, TicketsModule, ChinhSachModule],
+  imports: [PrismaModule, TicketsModule, ChinhSachModule, BaoCaoModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
